4a2879502394afc5992a0be2b2ad8e2e644be704
sfleussu <serge.fleussu@cfwb.be>
2018-02-14 06:58:46 +0100

    Testing JS

--- frontpage.php
+++ frontpage.php
@@ -1,1 +1,7 @@
+<div class="jumbotron text-center">
+  <h1>Welcome to my Moodle</h1>
+  <p>A responsive Moodle front page insert</p> 
+   <br /><a href="#" data-toggle="popover" title="Contact" 
+      data-content="email: us@muddle.com">Contact us</a>
+</div>
 <h1>Great !</h1>
